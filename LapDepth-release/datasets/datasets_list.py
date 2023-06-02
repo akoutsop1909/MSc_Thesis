dd5110a2596417d95b5113aaf570a9266b2708d6
Angel Kou <51194516+akoutsop1909@users.noreply.github.com>
2023-06-02 18:26:38 +0300

    resize

--- LapDepth-release/datasets/datasets_list.py
+++ LapDepth-release/datasets/datasets_list.py
@@ -151,7 +151,8 @@ class Transformer(object):
     def __init__(self, args):
         if args.dataset == 'KITTI':
             self.train_transform = EnhancedCompose([
-                RandomCropNumpy((args.height,args.width)),
+                #RandomCropNumpy((args.height,args.width)),
+                transforms.Resize((args.height, args.width)),
                 RandomHorizontalFlip(),
                 [RandomColor(multiplier_range=(0.9, 1.1)), None, None],
                 ArrayToTensorNumpy(),
