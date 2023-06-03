740c41760d57424873bfced85b0ad75c6203b02c
Angel Kou <51194516+akoutsop1909@users.noreply.github.com>
2023-06-03 12:59:00 +0300

    Update datasets_list.py

--- LapDepth-release/datasets/datasets_list.py
+++ LapDepth-release/datasets/datasets_list.py
@@ -161,7 +161,7 @@ class Transformer(object):
             """
             self.train_transform = EnhancedCompose([
                 #RandomCropNumpy((args.height,args.width)),
-                Resize((args.height,args.width)),
+                Resize(args.height,args.width),
                 RandomHorizontalFlip(),
                 [RandomColor(multiplier_range=(0.9, 1.1)), None, None],
                 ArrayToTensorNumpy(),
