b90485d1b8046b0029c6b4e0975c44841b971c43
Angelos K <51194516+akoutsop1909@users.noreply.github.com>
2024-03-14 15:51:33 +0200

    upload models

--- models/BANet/main.py
+++ models/BANet/main.py
@@ -189,6 +189,8 @@ def main():
         if not os.path.exists('prediction'):
             os.makedirs('prediction')
 
+        df = pd.read_csv(args.val_csv)
+
         # inference on random images from val dataset
         for i in range(50):
             # select a random image
@@ -202,22 +204,16 @@ def main():
             prediction = inference.predict_single(img)
             duration = inference.get_last_prediction_time()
 
-            """
-            # save prediction for diode
-            df = pd.read_csv('datasets/diode_val.csv')
-            temp = df['depth_path'][idx].split('/')
-            save('prediction/' + temp[7], prediction)
-
-            # save prediction for kitti selection
-            df = pd.read_csv('datasets/kitti_selection.csv')
-            temp = df['depth_path'][idx].split('/')
-            save('prediction/' + temp[4], prediction)
-            """
-
-            # save prediction for dipae
-            df = pd.read_csv('datasets/dipae.csv')
-            temp = df['raw_path'][idx].split('/')
-            save('prediction/' + temp[3][:-4], prediction)
+            # save prediction
+            if args.val_csv == 'datasets/diode_val.csv':
+                temp = df['depth_path'][idx].split('/')
+                save('prediction/' + temp[7], prediction)
+            elif args.val_csv == 'datasets/kitti_selection.csv':
+                temp = df['depth_path'][idx].split('/')
+                save('prediction/' + temp[7], prediction)
+            else:
+                temp = df['raw_path'][idx].split('/')
+                save('prediction/' + temp[3][:-4], prediction)
 
             # print last elapsed time
             print("Inference time: {:.4f} sec".format(duration))
